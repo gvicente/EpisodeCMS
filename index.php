25c20d19e847c8311d303d69c5a1c2293ebb1fdd
Aleksey Razbakov <razbakov.aleksey@gmail.com>
2010-12-10 11:01:31 +0200

    Deleted temp and config

--- index.php
+++ index.php
@@ -1,31 +1,30 @@
 <?php
-	define('DS', DIRECTORY_SEPARATOR);
-	define('ROOT', dirname(__FILE__));
-	define('APP_DIR', '.');
-	define('CAKE_CORE_INCLUDE_PATH', ROOT. DS.'vendors'.DS);
-	define('WEBROOT_DIR', basename(dirname(__FILE__)));
-	define('WWW_ROOT', dirname(__FILE__) . DS);
-	define('APP_PATH', ROOT . DS . APP_DIR . DS);
-	define('CORE_PATH', CAKE_CORE_INCLUDE_PATH);
-	define('CONFIGS', CAKE_CORE_INCLUDE_PATH.'cakeconfig'.DS);
-	define('TEMP', ROOT.DS.'tmp'.DS);
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('APP_DIR', '.');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendors' . DS);
+define('WEBROOT_DIR', basename(dirname(__FILE__)));
+define('WWW_ROOT', dirname(__FILE__) . DS);
+define('APP_PATH', ROOT . DS . APP_DIR . DS);
+define('CORE_PATH', CAKE_CORE_INCLUDE_PATH);
+define('CONFIGS', CAKE_CORE_INCLUDE_PATH . 'cakeconfig' . DS);
+define('TEMP', ROOT . DS . 'tmp' . DS);
 
-	foreach(array(
-		TEMP, 
-		TEMP.'cache',
-		TEMP.'cache'.DS.'persistent',
-		TEMP.'cache'.DS.'models',
-		TEMP.'cache'.DS.'views'		
-	) as $folder) {
-		file_exists($folder) || mkdir($folder, 0777, true); 
-	} 	
-	
-	include(CORE_PATH . 'cake' . DS . 'bootstrap.php');
+foreach (array(
+    TEMP,
+    TEMP . 'cache',
+    TEMP . 'cache' . DS . 'persistent',
+    TEMP . 'cache' . DS . 'models',
+    TEMP . 'cache' . DS . 'views'
+) as $folder) {
+    file_exists($folder) || mkdir($folder, 0777, true);
+}
 
-	$Dispatcher = new Dispatcher();
-	$Dispatcher->dispatch();
+include(CORE_PATH . 'cake' . DS . 'bootstrap.php');
 
-	if (Configure::read() > 0) {
-		echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
-	}
-?>
+$Dispatcher = new Dispatcher();
+$Dispatcher->dispatch();
+
+if (Configure::read() > 0) {
+    echo "<!-- " . round(getMicrotime() - $TIME_START, 4) . "s -->";
+}
